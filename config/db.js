9112612f280f764488d679d410087b5cb4c6cdd9
Jaylian096 <jaylianbacolod096@gmail.com>
2026-03-20 20:58:36 +0800

    new

--- config/db.js
+++ config/db.js
@@ -2,16 +2,15 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: process.env.MYSQLHOST || 'localhost',
-  user: process.env.MYSQLUSER || 'root',
-  password: process.env.MYSQLPASSWORD || '',
-  database: process.env.MYSQLDATABASE || 'bantayan_ferry',
-  port: process.env.MYSQLPORT || 3306,
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || '',
+  database: process.env.DB_NAME || 'bantayan_ferry',
+  port: process.env.DB_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
 
 const db = pool.promise();
-
 module.exports = db;
