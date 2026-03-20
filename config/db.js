d526767ee33af4c03adb1d828531f2dd620b8e69
Jaylian096 <jaylianbacolod096@gmail.com>
2026-03-20 20:53:40 +0800

    new

--- config/db.js
+++ config/db.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
+  host: process.env.DB_HOST || 'mysql.railway.internal',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'bantayan_ferry',
+  password: process.env.DB_PASSWORD || 'KTwgaCcQavQDlkTYsIDyDlZBpitAIhKo',
+  database: process.env.DB_NAME || 'railway',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
